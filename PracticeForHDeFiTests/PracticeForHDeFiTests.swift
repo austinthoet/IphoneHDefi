25301328c82ca61755b05d259f011daccb1e8073
Austin Thoet <austinthoet@Austins-MacBook-Pro.local>
2017-09-04 23:28:33 -0400

    Initial Commit

---
+++ PracticeForHDeFiTests/PracticeForHDeFiTests.swift
@@ -0,0 +1,36 @@
+//
+//  PracticeForHDeFiTests.swift
+//  PracticeForHDeFiTests
+//
+//  Created by Austin Thoet on 9/4/17.
+//  Copyright © 2017 Austin Thoet. All rights reserved.
+//
+
+import XCTest
+@testable import PracticeForHDeFi
+
+class PracticeForHDeFiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
